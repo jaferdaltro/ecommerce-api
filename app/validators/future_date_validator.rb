e838ebc0ccb7dd2fd2d55446bd24c5f7067c53b2
Jafer Daltro <jafer.daltro@gmail.com>
2022-01-16 13:36:56 -0300

    add future_date_validator

--- app/validators/future_date_validator.rb
+++ app/validators/future_date_validator.rb
@@ -1,2 +1,8 @@
-class FutureValidator < ActiveModel::EachValidator
+class FutureDateValidator < ActiveModel::EachValidator
+  def validate_each(record, attrubute, value)
+    if value.present? && value <= Time.zone.now
+      message = options[:message] || :future_date
+      record.errors.add(attrubute, message)
+    end
+  end
 end
