5bfd87d3325d20903fbac12a12d542ff08676f9d
Jafer Daltro Pompeu Jr <jafer.daltro@gmail.com>
2022-04-02 16:05:17 -0300

    fix typo

--- spec/services/admin/model_loading_service_spec.rb
+++ spec/services/admin/model_loading_service_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe Admin::ModelLoadingService do
   context "when #call" do
-    let!(:categories) { create_list(:category, 15)}
+    let!(:categories) { create_list(:category, 15) }
 
     context "when params are present" do
       let!(:search_categories) do
@@ -11,10 +11,10 @@ describe Admin::ModelLoadingService do
         categories
       end
       let(:params) do
-        { seach: {name: "Search"},order: {name: :desc} , page:2 ,lenght: 4}
+        { seach: { name: "Search" },order: { name: :desc } , page: 2 ,length: 4 }
       end
 
-      it "returns right :lenght following pagination" do
+      it "returns right :length following pagination" do
         service = described_class.new(Category.all, params)
         result_categories = service.call
         expect(result_categories.count).to eq 4
