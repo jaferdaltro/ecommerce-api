aedb4e35f6c49408ffa86787466fb9a2220956b8
Jafer Daltro Pompeu Jr <jafer.daltro@gmail.com>
2022-04-02 16:05:01 -0300

    add status to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,10 +6,13 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :status, presence: true
 
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
 
   has_one_attached :image
   validates :image, presence: true
+
+  enum status: { unavailable: 0, available: 1 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_15_212856) do
+ActiveRecord::Schema.define(version: 2022_04_02_183353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2022_01_15_212856) do
     t.bigint "productable_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status"
     t.index ["productable_type", "productable_id"], name: "index_products_on_productable_type_and_productable_id"
   end
 
