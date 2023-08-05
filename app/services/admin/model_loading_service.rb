9046a1d0218d0cdc21ea57ac9c8be785e1efee31
Jafer Daltro <jafer.daltro@gmail.com>
2023-08-05 16:52:05 -0300

    Refectory Model Loading Service test

--- app/services/admin/model_loading_service.rb
+++ app/services/admin/model_loading_service.rb
@@ -1,44 +1,27 @@
 module Admin
   class ModelLoadingService
     attr_reader :records, :pagination
-
     def initialize(searchable_model, params = {})
       @searchable_model = searchable_model
       @params = params || {}
       @records = []
-      @pagination = {}
+      @pagination = { page: @params[:page].to_i, length: @params[:length].to_i }
     end
 
     def call
-      set_pagination_values
-      searched = search_records(@searchable_model)
-      @records = searched.order(@params[:order].to_h)
-                         .paginate(@params[:page], @params[:length])
-      set_pagination_attributes(searched.count)
+      fix_pagination_values
+      filtered = @searchable_model.search_by_key(@params.dig(:search, :name))
+      @records = filtered.order(@params[:order].to_h).paginate(@pagination[:page],
+        @pagination[:length])
+      total_pages = (filtered.count / @pagination[:length].to_f).ceil
+      @pagination.merge!(total: filtered.count, total_pages: total_pages )
     end
 
     private
 
-    def set_pagination_values
-      @params[:page] = @params[:page].to_i
-      @params[:length] = @params[:length].to_i
-      @params[:page] = @searchable_model.model::DEFAULT_PAGE if @params[:page] <= 0
-      @params[:length] = @searchable_model.model::MAX_PER_PAGE if @params[:length] <= 0
-    end
-
-    def search_records(searched)
-      return searched unless @params.has_key?(:search)
-
-      @params[:search].each do |key, value|
-        searched = searched.like(key, value)
-      end
-      searched
-    end
-
-    def set_pagination_attributes(total_filtered)
-      total_pages = (total_filtered / @params[:length].to_f).ceil
-      @pagination.merge!(page: @params[:page], length: @records.count, 
-                         total: total_filtered, total_pages: total_pages)
+    def fix_pagination_values
+      @pagination[:page] = @searchable_model.model::DEFAULT_PAGE if @pagination[:page] <= 0
+      @pagination[:length] = @searchable_model.model::MAX_PER_PAGE if @pagination[:length] <= 0
     end
   end
 end
