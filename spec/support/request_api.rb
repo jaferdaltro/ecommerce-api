5fc7208a59590da9fc065d77c892b0181c59d51f
Jafer Daltro <jafer.daltro@gmail.com>
2024-02-02 09:59:43 -0300

    fix product delete test.

--- spec/support/request_api.rb
+++ spec/support/request_api.rb
@@ -2,6 +2,8 @@ module RequestAPI
   def body_json(symbolize_keys: false)
     json = JSON.parse(response.body)
     symbolize_keys ? json.deep_symbolize_keys : json
+  rescue
+    {}
   end
 
   def auth_header(user = nil, merge_with: {})
